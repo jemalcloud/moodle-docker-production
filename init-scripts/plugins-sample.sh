7a4eb79127d03cc51b009f93b36abae008fe2010
deleyva <jesuslopezdeleyva@gmail.com>
2020-05-19 16:53:13 +0200

    configurando moodle

--- init-scripts/plugins-sample.sh
+++ init-scripts/plugins-sample.sh
@@ -14,13 +14,30 @@ echo >&2 "Plugin list downloaded!"
 
 
 echo >&2 "Installing plugins..."
-moosh plugin-install -d --release 2019051505 theme_snap
-moosh plugin-install -d --release 2019042008 mod_bigbluebuttonbn
-moosh plugin-install -d --release 2020020500 mod_hvp
-moosh plugin-install -d --release 2020043003 block_xp
+moosh plugin-install -d theme_snap
+moosh plugin-install -d mod_hvp
+moosh plugin-install -d block_xp
 moosh plugin-install -d availability_xp 
 echo >&2 "Plugins installed!"
 
 moosh config-set theme snap
-moosh config-set bigbluebuttonbn_server_url 2.2.2.2
-moosh config-set bigbluebuttonbn_shared_secret thisIsMySecret
+moosh config-set tool_generator_users_password ${VARIABLE_DE_ENTORNO}
+moosh config-set smtphosts ${VARIABLE_DE_ENTORNO}
+moosh config-set smtpsecure ${VARIABLE_DE_ENTORNO}
+moosh config-set smtpauthtype LOGIN
+moosh config-set smtpuser ${VARIABLE_DE_ENTORNO}
+moosh config-set smtppass ${VARIABLE_DE_ENTORNO}
+moosh config-set smtpmaxbulk 100
+moosh config-set noreplyaddress ${VARIABLE_DE_ENTORNO}
+moosh config-set cron_hour 0
+moosh config-set cron_minute 0
+moosh config-set crrepository jleyva/moodle-configurable_reports_repository
+moosh config-set dbhost ${VARIABLE_DE_ENTORNO}
+moosh config-set dbname ${VARIABLE_DE_ENTORNO}
+moosh config-set dbpass ${VARIABLE_DE_ENTORNO}
+moosh config-set dbuser ${VARIABLE_DE_ENTORNO}
+moosh config-set reportlimit 5000
+moosh config-set reporttableui datatables
+moosh config-set sharedsqlrepository jleyva/moodle-custom_sql_report_queries
+moosh config-set sqlsecurity 1
+moosh config-set sqlsyntaxhighlight 1
