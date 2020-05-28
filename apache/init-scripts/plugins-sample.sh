fa1151dee35821b17e2b7cce8c4e060b28bfb579
deleyva <jesuslopezdeleyva@gmail.com>
2020-05-28 12:08:26 +0200

    voy añadiendo configs

--- apache/init-scripts/plugins-sample.sh
+++ apache/init-scripts/plugins-sample.sh
@@ -2,7 +2,7 @@
 # This script contains commands that should be executed first 
 # time the containers goes up or after upgrades to update database
 
-# example command: moosh -n config-set name value <plugin>
+# example command: moosh config-set name value <plugin>
 
 # Plugin list with [--release <build version>] if different from last
 # hack: last one does never get active, so install one more
@@ -62,15 +62,36 @@ moosh config-set sqlsecurity 1 block_configurable_reports
 moosh config-set sqlsyntaxhighlight 1 block_configurable_reports
 
 # Config webservices #TODO check config
-moosh -n config-set enablewebservices 1 core
-moosh -n config-set enablemobilewebservice 1 core 
+moosh config-set enablewebservices 1 core
+moosh config-set enablemobilewebservice 1 core 
 
 # Config mobile notifications #TODO check config
-moosh -n config-set airnotifierurl https://messages.moodle.net
-moosh -n config-set airnotifierport 443
-moosh -n config-set airnotifiermobileappname com.moodle.moodlemobile
-moosh -n config-set airnotifierappname commoodlemoodlemobile
-moosh -n config-set airnotifieraccesskey #TODO
+# moosh config-set airnotifierurl https://messages.moodle.net
+# moosh config-set airnotifierport 443
+# moosh config-set airnotifiermobileappname com.moodle.moodlemobile
+# moosh config-set airnotifierappname commoodlemoodlemobile
+# moosh config-set airnotifieraccesskey #TODO
+# 
+# digestmailtime
+# registrationpending {int de 24 horas}
+# doclang es
+# defaulthomepage 1
+# allowthemechangeonurl 1 # cambiar a 0
+# themedesignermode 1 # cambiar a 0
+# cronremotepassword klavekron # mejorar
+# cronclionly # ¿Qué es esto?
+# searchincludeallcourses 1
+# lang es
+# country ES
+# timezone Europe/Madrid
+# badges_allowexternalbackpack 1
+# badges_allowcoursebadges 1
+# badges_badgesalt badges1589239382 
+# badges_defaultissuercontact ${MOODLE_MANAGER}
+# 
+# enablegravatar 1
+
+# guestloginbutton puede ser que esto lo quieran quitar
 
 set +x
 
@@ -84,10 +105,10 @@ moosh config-set theme moove
 # tar -zcf snap_settings.tar.gz -C /init-scripts/snap_settings .
 
 # find /init-scripts/snap_settings -type f -printf "%f\n" | xargs tar -zcf snap_settings.tar.gz -C /init-scripts/snap_settings
-# moosh -n theme-settings-import --targettheme snap snap_settings.tar.gz
+# moosh theme-settings-import --targettheme snap snap_settings.tar.gz
 cp /init-scripts/*tar.gz /var/www/html/
-moosh -n theme-settings-import --targettheme moove *tar.gz
+moosh theme-settings-import --targettheme moove *tar.gz
 
 
-# moosh -n config-set bigbluebuttonbn_server_url 2.2.2.2
-# moosh -n config-set bigbluebuttonbn_shared_secret thisIsMySecret
+# moosh config-set bigbluebuttonbn_server_url 2.2.2.2
+# moosh config-set bigbluebuttonbn_shared_secret thisIsMySecret
