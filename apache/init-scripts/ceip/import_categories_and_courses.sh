85428845a9984897c1e941e39dca3405f722f359
deleyva <jesuslopezdeleyva@gmail.com>
2020-06-02 22:41:44 +0200

    infantil

--- apache/init-scripts/ceip/import_categories_and_courses.sh
+++ apache/init-scripts/ceip/import_categories_and_courses.sh
@@ -45,6 +45,19 @@ moosh -n course-config-set course 25 shortname Inf_Seg_Aleman
 moosh -n course-config-set course 26 shortname Inf_Seg_Frances
 moosh -n course-config-set course 27 shortname Inf_Seg_Aragones
 moosh -n course-config-set course 28 shortname Inf_Seg_Catalan
+moosh -n course-config-set course 29 shortname Inf_Ter_Proyecto1
+moosh -n course-config-set course 30 shortname Inf_Ter_Proyecto2
+moosh -n course-config-set course 31 shortname Inf_Ter_Proyecto3
+moosh -n course-config-set course 32 shortname Inf_Ter_Proyecto4
+moosh -n course-config-set course 33 shortname Inf_Ter_Proyecto5
+moosh -n course-config-set course 34 shortname Inf_Ter_Proyecto6
+moosh -n course-config-set course 35 shortname Inf_Ter_English
+moosh -n course-config-set course 36 shortname Inf_Ter_Atencion
+moosh -n course-config-set course 37 shortname Inf_Ter_Religion
+moosh -n course-config-set course 38 shortname Inf_Ter_Aleman
+moosh -n course-config-set course 39 shortname Inf_Ter_Frances
+moosh -n course-config-set course 40 shortname Inf_Ter_Aragones
+moosh -n course-config-set course 41 shortname Inf_Ter_Catalan
 
 moosh -n course-config-set course 3 fullname "Proyecto 1"
 moosh -n course-config-set course 4 fullname "Proyecto 2"
@@ -72,7 +85,36 @@ moosh -n course-config-set course 25 fullname Alemán
 moosh -n course-config-set course 26 fullname Francés
 moosh -n course-config-set course 27 fullname Aragonés
 moosh -n course-config-set course 28 fullname Catalán
+moosh -n course-config-set course 29 fullname "Proyecto 1"
+moosh -n course-config-set course 30 fullname "Proyecto 2"
+moosh -n course-config-set course 31 fullname "Proyecto 3"
+moosh -n course-config-set course 32 fullname "Proyecto 4"
+moosh -n course-config-set course 33 fullname "Proyecto 5"
+moosh -n course-config-set course 34 fullname "Proyecto 6"
+moosh -n course-config-set course 35 fullname English
+moosh -n course-config-set course 36 fullname "Atención Educativa"
+moosh -n course-config-set course 37 fullname Religión
+moosh -n course-config-set course 38 fullname Alemán
+moosh -n course-config-set course 39 fullname Francés
+moosh -n course-config-set course 40 fullname Aragonés
+moosh -n course-config-set course 41 fullname Catalán
 
 # Importing courses in pri
 categories="31 32 33 34 35 36"
+
+for category in $categories; do
+  moosh -n course-restore mbzs/copia_de_seguridad-moodle2-course-10-20200530-0857-nu.mbz $category
+  moosh -n course-restore mbzs/copia_de_seguridad-moodle2-course-13-20200530-0857-nu.mbz $category
+  moosh -n course-restore mbzs/copia_de_seguridad-moodle2-course-14-20200530-0857-nu.mbz $category
+  moosh -n course-restore mbzs/copia_de_seguridad-moodle2-course-15-20200530-0857-nu.mbz $category
+  moosh -n course-restore mbzs/copia_de_seguridad-moodle2-course-16-20200530-0857-nu.mbz $category
+  moosh -n course-restore mbzs/copia_de_seguridad-moodle2-course-17-20200530-0857-nu.mbz $category
+  moosh -n course-restore mbzs/copia_de_seguridad-moodle2-course-18-20200530-0857-nu.mbz $category
+  moosh -n course-restore mbzs/copia_de_seguridad-moodle2-course-19-20200530-0857-nu.mbz $category
+  moosh -n course-restore mbzs/copia_de_seguridad-moodle2-course-21-20200530-0857-nu.mbz $category
+  moosh -n course-restore mbzs/copia_de_seguridad-moodle2-course-22-20200530-0857-nu.mbz $category
+  moosh -n course-restore mbzs/copia_de_seguridad-moodle2-course-23-20200530-0857-nu.mbz $category
+  moosh -n course-restore mbzs/copia_de_seguridad-moodle2-course-24-20200530-0857-nu.mbz $category
+  moosh -n course-restore mbzs/copia_de_seguridad-moodle2-course-25-20200530-0857-nu.mbz $category
+done
 
