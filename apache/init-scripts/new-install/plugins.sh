39dbe53e5d4046060ec0013103c572d7d7388aba
deleyva <jesuslopezdeleyva@gmail.com>
2020-06-04 16:31:20 +0200

    create categories in sh for CEIP

--- apache/init-scripts/new-install/plugins.sh
+++ apache/init-scripts/new-install/plugins.sh
@@ -43,10 +43,10 @@ set +x # to get info
 moosh config-set cron_hour 0
 moosh config-set cron_minute 0
 moosh config-set crrepository jleyva/moodle-configurable_reports_repository block_configurable_reports
-moosh config-set dbhost db block_configurable_reports
-moosh config-set dbname moodle block_configurable_reports
-moosh config-set dbpass dbpassword block_configurable_reports
-moosh config-set dbuser dbuser block_configurable_reports
+moosh config-set dbhost ${MOODLE_DB_HOST} block_configurable_reports
+moosh config-set dbname ${MOODLE_DB_NAME} block_configurable_reports
+moosh config-set dbuser ${MOODLE_DB_USER} block_configurable_reports
+moosh config-set dbpass ${MOODLE_DB_PASSWORD} block_configurable_reports
 moosh config-set reportlimit 5000 block_configurable_reports
 moosh config-set reporttableui datatables block_configurable_reports
 moosh config-set sharedsqlrepository jleyva/moodle-custom_sql_report_queries block_configurable_reports
