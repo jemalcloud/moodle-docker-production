4fe528e855be857f491485a36595a0f3889a4244
juanda <juandacorreo@gmail.com>
2020-07-02 00:43:50 +0200

    fix center creation

--- data/index.js
+++ data/index.js
@@ -1,18 +1,41 @@
-const csvFilePath = './centros.csv'
-const csv = require('csvtojson')
+var gsjson = require('google-spreadsheet-to-json');
 const fs = require("fs");
-const writeStream = fs.createWriteStream('createCenters.sh');
-const pathName = writeStream.path;
-csv()
-  .fromFile(csvFilePath)
-  .then((jsonObj) => {
-    const centros = jsonObj.map((centro) => {
-      const url = `https://${centro.url}.aeducar.es`
-      const shortName = centro.longName.substring(0, centro.longName.indexOf("(") - 1)
-      return { url, shortName, longName: centro.longName, deployType: centro.deployType }
-    }).filter(centro => centro.deployType !== 'VACIO')
 
-    centros.forEach(centro => writeStream.write(`createMoodle.sh -l es -n "${centro.longName}" -t "${centro.deployType}" -u "${centro.url}" "${centro.shortName}" \nsleep 5m\n`));
+
+const myArgs = process.argv.slice(2);
+
+switch (myArgs[0]) {
+  case '1':
+  case '2':
+  case '3':
+    var fileName = `createCenters_${myArgs[0]}.sh`
+    console.log(`Data will be saved to ${fileName}`)
+    break;
+  default:
+    console.log('USAGE: node index.js <número servidor>');
+    process.exit();
+}
+
+const writeStream = fs.createWriteStream(fileName);
+const pathName = writeStream.path;
+
+gsjson({
+  spreadsheetId: '1SQjFfnTrMeGh281k0fAcDIL5oK_Kc2BR91Upr8kUU0E',
+  credentials: './credentials.json',
+  worksheet: 'junio'
+})
+  .then(function (jsonData) {
+    const centros = jsonData
+      .filter((centro) => centro.desplieguePrevisto !== 'desplegado')
+      .filter((centro) => !!centro.gfp_url_solicitada)
+      .filter((centro) => centro.servidor == myArgs[0])
+      .map((centro) => {
+        const url = `https://${centro.gfp_url_solicitada}.aeducar.es`
+        const shortName = centro.gfp_centro_y_localidad.substring(0, centro.gfp_centro_y_localidad.indexOf("(") - 1)
+        return { url, shortName, longName: centro.gfp_centro_y_localidad, deployType: centro.tipoDespliegue }
+      })
+    console.log(`Se han generado un total de ${centros.length} centros`);
+    centros.forEach(centro => writeStream.write(`createMoodle.sh -l es -n "${centro.longName}" -t "${centro.deployType}" -u "${centro.url}" "${centro.shortName}" \nsleep 4m\n`));
 
     // the finish event is emitted when all data has been flushed from the stream
     writeStream.on('finish', () => {
@@ -27,3 +50,7 @@ csv()
     // close the stream
     writeStream.end();
   })
+  .catch(function (err) {
+    console.log(err.message);
+    console.log(err.stack);
+  });
